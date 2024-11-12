b6103fa25ef55d01705f85c1a93de5185cb01bc0
annalisa4sec <122273976+annalisa4sec@users.noreply.github.com>
2024-11-12 10:43:50 +0100

    Add mongodb and mongoose, create post

--- routes/api.js
+++ routes/api.js
@@ -3,25 +3,47 @@
 module.exports = function (app) {
 
   app.route('/api/issues/:project')
-  
-    .get(function (req, res){
+
+    .get(function (req, res) {
       let project = req.params.project;
-      
+
     })
-    
-    .post(function (req, res){
-      let project = req.params.project;
-      
+
+    .post(async (req, res) => {
+      const project = req.params.project;
+      const { issue_title, issue_text, created_by, assigned_to, status_text } = req.body;
+      if (!issue_title || !issue_text || !created_by) {
+        return res.status(400).json({ error: "Required fields missing" });
+      }
+
+      try {
+        const issue = new Issue({
+          issue_title,
+          issue_text,
+          created_by,
+          assigned_to: assigned_to || '',
+          status_text: status_text || '',
+          created_on: new Date(),
+          updated_on: new Date(),
+          open: true
+        })
+
+        const savedIssue = await issue.save();
+        res.status(201).json(savedIssue);
+      } catch (err) {
+        console.log("There was an error while creating issue: ", err)
+      }
+      res.status(201).json(issue);
     })
-    
-    .put(function (req, res){
+
+    .put(function (req, res) {
       let project = req.params.project;
-      
+
     })
-    
-    .delete(function (req, res){
+
+    .delete(function (req, res) {
       let project = req.params.project;
-      
+
     });
-    
+
 };
